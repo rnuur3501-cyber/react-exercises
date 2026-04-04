6d41095356e905ac38455daaf74a3852389f50ab
Rahma Nor Abdirahman <rnuur3501@gmail.com>
2026-04-04 09:50:33 +0300

    first commit

---
+++ exercise3/exercise3/src/App.jsx
@@ -0,0 +1,12 @@
+import UserCard from './UserCard';
+
+const App = () => {
+  return (
+    <div>
+      <UserCard name="John Doe" email="john.doe@example.com" />
+      <UserCard name="Jane Smith" email="jane.smith@example.com" />
+    </div>
+  );
+};
+
+export default App;
